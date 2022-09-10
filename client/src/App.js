04d627b7b60f1ce7ad8f5743c8e6d23442a4c139
chad hinds <chadrickj8@gmail.com>
2022-09-10 11:42:54 -0400

    Put Message and ProgressBar component in render

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className='container'>
+    <div className='container mt-4'>
       <h4 className="display-4 text-center mb-4">
         <i className="fab fa-react" /> React File Upload
       </h4>
